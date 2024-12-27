387fe4365e33843bd1b02bc02d6e482f3da956b6
Jac <121095246+JacintaSyilloam@users.noreply.github.com>
2024-12-27 20:19:31 +0700

    wordlists.sh

---
+++ wordlists.sh
@@ -0,0 +1,2 @@
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-1000.txt
+wget https://raw.githubusercontent.com/danielmiessler/SecLists/master/Passwords/Common-Credentials/10-million-password-list-top-10000.txt
